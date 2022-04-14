f531011090ba5aab37af0b678c8300537ae2ff2a
mori <hakairyuousinn@yahoo.co.jp>
2022-04-14 22:14:17 +0900

    javafiles新規追加

---
+++ javafiles/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import issues.Bond;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Hello Innovation");
+        Bond bond = new Bond();
+    }
+}
