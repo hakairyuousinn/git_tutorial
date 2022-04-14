a51f146d20222253915e5d21f666cb169c5f8ee6
mori <hakairyuousinn@yahoo.co.jp>
2022-04-14 22:49:06 +0900

    murimuri

---
+++ javafiles/src/issues/Stock.java
@@ -0,0 +1,7 @@
+package issues;
+
+public class Stock {
+    public static void main(String[] args) {
+        System.out.println("Stock Test");
+    }
+}
---
+++ javafiles/src/issuescopy/Bond.java
@@ -0,0 +1,19 @@
+package issuescopy;
+
+public class Bond{
+    public static void main(String[] args) {
+        System.out.println("Hello World, Hello Innovation");
+        //Bond bond = new Bond();
+        //bond.setName("name");
+        //System.out.println(bond.getName());
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
